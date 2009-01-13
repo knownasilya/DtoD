6c54a7f27ff16f9f0ab0eef932e7cd6307ce16d2
knownasilya <knownasilya@6ee8ad01-c9c3-4015-853b-cb85d61bb0b1>
2009-01-13 05:36:12 +0000

    Updated the events index page with hcalender events specifications.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :name, :description, :location, :event_type, :event_date
+  attr_accessible :name, :description, :location, :event_type, :event_date, :price, :website, :end_date
   
   validates_presence_of :event_date, :name
   validates_length_of :name, :minimum => 3
